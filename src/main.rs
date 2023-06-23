c2c6b0a7455cf7d2d490455ca1ffa1a611d1527f
calvin <clui@itbit.com>
2023-06-22 21:27:26 -0400

    vendor fortytwo

--- src/main.rs
+++ src/main.rs
@@ -1,11 +1,13 @@
 use std::thread;
 use std::time::Duration;
 use rand::random;
+use fortytwo::number;
 
 fn main() {
     loop {
         let n = random::<u32>();
-        println!("n: {}", n);
+        let m = number();
+        println!("n: {} m: {}", n, m);
         thread::sleep(Duration::from_secs(1));
     }
 }
